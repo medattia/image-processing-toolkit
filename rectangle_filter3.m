773692ebec02224c4cc33eb82e6792732352e5c4
medattia <42179315+medattia@users.noreply.github.com>
2020-06-09 22:26:04 +0100

    Delete rectangle_filter3.m

--- rectangle_filter3.m
+++
@@ -1,9 +0,0 @@
-function result = rectangle_filter3(vertical, horizontal)
-
-% function result = rectangle_filter3(vertical, horizontal)
-%
-%  creates a rectangle filter of type 3 
-% (white on the left, black on the center, white on the right).
-
-result = ones(vertical, 3 * horizontal);
-result(:, (horizontal+1):(2 * horizontal)) = -2;
