054de1638f1bada1ded07324df601ec90c9d1410
medattia <42179315+medattia@users.noreply.github.com>
2020-06-13 13:36:37 +0100

    Delete make.m

--- make.m
+++
@@ -1,4 +0,0 @@
-fprintf('Mexing mexColorNaming...\n'); mex mexColorNaming.c
-fprintf('Mexing mexHueDescriptor...\n'); mex mexHueDescriptor.c
-fprintf('Mexing mexOpponent...\n'); mex mexOpponent.c
-fprintf('Done.\n');
