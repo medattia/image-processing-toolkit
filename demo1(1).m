2bb47a32542033335e7bf33a3512ba737024f8e3
medattia <42179315+medattia@users.noreply.github.com>
2020-06-22 22:45:57 +0100

    Delete demo1(1).m

--- demo1(1).m
+++
@@ -1,9 +0,0 @@
-%% First demo, very simple, datasize: 10, 
-%% until zero training error
-%% draw emphasized characters
-%% step by step.
-
-load demo1.mat
-
-[w, h] = ...
-    adabstdemo(x, y, 10, @stumplearn, @stumpfunc, 0, 1, 1);
