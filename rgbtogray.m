644f057eb2c7324e0076bdef7e830931ac3f26cc
medattia <42179315+medattia@users.noreply.github.com>
2020-06-23 23:43:45 +0100

    Update rgbtogray.m

--- rgbtogray.m
+++ rgbtogray.m
@@ -1,25 +1,69 @@
+
+%Fonction qui transforme une image RGB en Nuance de gris NG avec trois pondérations différentes 
+%semblables à celles de matlab
+function RGBTOGRAY3()
+[fichier, chemin] = uigetfile('*.*','Choisir l''image:');
+if ~fichier
+   error('Aucun fichier n''a été désigné !')
+end
+%Lecture de l'image
+Mat=imread(fichier);
+R = Mat(:,:,1);
+G = Mat(:,:,2);
+B = Mat(:,:,3);
+%Calcul
+NG=uint8(0.2125*R+0.7159*G+0.0721*B);
+%Visualisation
+subplot(1,2,1)
+imshow(Mat);
+title('Image initiale ');
+subplot(1,2,2)
+imshow(NG);
+title('Image en nuances de gris');
+end
+
+%Fonction qui transforme une image RGB en Nuance de gris NG comme moyenne des trois intensités
+function RGBTOGRAY1()
+[fichier, chemin] = uigetfile('*.*','Choisir l''image:');
+if ~fichier
+   error('Aucun fichier n''a été désigné !')
+end
+%Lecture de l'image
+Mat=imread(fichier); 
+R = Mat(:,:,1);
+G = Mat(:,:,2);
+B = Mat(:,:,3);
+%Calcul
+NG=(R+G+B)/3;
+%Visualisation
+subplot(1,2,1)
+imshow(Mat);
+title('Image initiale ');
+subplot(1,2,2)
+imshow(NG);
+title('Image en nuances de gris');
+end 
+
+
 %Fonction qui transforme une image RGB en Nuance de gris NG avec trois pondérations différentes
-function ng=rgbtogray(mat)
-R=uint16(mat(:,:,1));
-G=uint16(mat(:,:,2));
-B=uint16(mat(:,:,3));
-ng=uint8(0.2125*R+0.7159*G+0.0721*B);
+function RGBTOGRAY2()
+[fichier, chemin] = uigetfile('*.*','Choisir l''image:');
+if ~fichier
+   error('Aucun fichier n''a été désigné !')
 end
-
-%Fonction qui transforme une image RGB en Nuance de gris NG avec moyenne des trois intensités
-function ng=rgbtogray1(mat)
-R=(mat(:,:,1));
-G=(mat(:,:,2));
-B=(mat(:,:,3));
-ng=((R+G+B)/3);
-end
-
-%Fonction qui transforme une image RGB en Nuance de gris NG avec trois pondérations différentes semblables à celles de matlab
-function ng=rgbtogray2(mat)
-R=(mat(:,:,1));
-G=(mat(:,:,2));
-B=(mat(:,:,3));
-ng=(0.299*R+0.587*G+0.114*B);
-end
-
+%Lecture de l'image
+Mat=imread(fichier); 
+R = Mat(:,:,1);
+G = Mat(:,:,2);
+B = Mat(:,:,3);
+%Calcul
+NG=0.212*R+0.716*G+0.072*B;
+%Visualisation
+subplot(1,2,1)
+imshow(Mat);
+title('Image initiale ');
+subplot(1,2,2)
+imshow(NG);
+title('Image en nuances de gris');
+end 
 
