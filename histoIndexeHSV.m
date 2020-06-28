83eb955922ae912a423567b9c4c38b763dde5800
medattia <42179315+medattia@users.noreply.github.com>
2020-06-28 12:07:38 +0100

    Delete histoIndexeHSV.m

--- histoIndexeHSV.m
+++
@@ -1,7 +0,0 @@
-% descripteur HSV indexe : QUATRIÈME descripteur
-function HHSV=histoIndexeHSV(im,QH,QS,QV)
-image_hsv=rgb2hsv(im);
-im_indexe_hsv=QNHSV(im,QH,QS,QV)
-Hhsv=histo(im_indexe_HSV,Qh*Qs*Qv)
-
-end
