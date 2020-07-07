f6d66935ba836e07b3876cac795a743c8a7d7dc1
medattia <42179315+medattia@users.noreply.github.com>
2020-07-07 23:36:46 +0100

    Update seuillage_nilback.m

--- seuillage_nilback.m
+++ seuillage_nilback.m
@@ -65,3 +65,16 @@ imshow(mat);
 title('image initial ');
 subplot(122) %image Finale
 imshow(imageNilback);
+
+function s=seuillagenilback(mat)
+s=zeros(size(mat,1),size(mat,2));
+seuil=median(mat)
+for i=1:size(mat,1);
+   for j=1:size(mat,2);
+     if mat(i,j)>seuil;
+       s(i,j)=1;
+     else s(i,j)=0;
+     end;
+   end;
+end;
+end
