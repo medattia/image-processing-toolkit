df18430d5f6deff74986d6e529b85e607007246b
medattia <42179315+medattia@users.noreply.github.com>
2020-07-09 22:58:00 +0100

    Update and rename SeuillageBernson.m to seuillage_bernson.m

--- seuillage_bernson.m
+++ seuillage_bernson.m
@@ -30,3 +30,16 @@ imshow(ImBerson);
 title('image binaire Bernson');
 end 
 
+
+function s=seuillagebernsen(mat)
+s=zeros(size(mat,1),size(mat,2));
+seuil=(max(mat)+min(mat))/2
+for i=1:size(mat,1);
+   for j=1:size(mat,2);
+     if mat(i,j)>seuil;
+       s(i,j)=1;
+     else s(i,j)=0;
+     end;
+   end;
+end;
+end
