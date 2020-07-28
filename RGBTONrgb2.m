701710876102f63c6ab0a99371d42322868cb7d4
medattia <42179315+medattia@users.noreply.github.com>
2020-07-28 23:21:44 +0100

    Update RGBTONrgb2.m

--- RGBTONrgb2.m
+++ RGBTONrgb2.m
@@ -35,3 +35,51 @@ imshow(Normal_RGB2);
 title('image normalisee 2');
 end 
 
+%Fonction qui transforme une image RGB en une image RGB normalisée
+function RGBTONrgb1()
+[fichier, chemin] = uigetfile('*.*','Choisir l''image:');
+if ~fichier
+   error('Aucun fichier n''a été désigné !')
+end
+%Lecture de l'image
+M = imread(fichier);
+
+R = double(M(:,:,1)/255); 
+G = double(M(:,:,2)/255); 
+B = double(M(:,:,3)/255); 
+
+%Calcul NRGB1 (RGB noralisee 1) ================
+SOM_RGB = double(R + G + B);
+r = double(R./SOM_RGB);
+g = double(G./SOM_RGB);
+b = double(B./SOM_RGB);
+
+    Normal_RGB(:,:,1)=r*255;
+    Normal_RGB(:,:,2)=g*255;
+    Normal_RGB(:,:,3)=b*255;
+    
+
+%Visualisation
+subplot(1,2,1)
+imshow(M);
+title('image initiele');
+subplot(1,2,2);
+imshow(Normal_RGB);
+title('image normalisee 1');
+end 
+function NRGB1=RGBTONRGB(mat)
+R=double(mat(:,:,1))/255;
+G=double(mat(:,:,2))/255;
+B=double(mat(:,:,3))/255;
+
+r=R/(R+G+B);
+g=G/(R+B+G);
+b=B/(R+G+B);
+
+NRGB1=uint8(zeros(size(mat)));
+
+NRGB(:,:,1)=uint8((r*R+g*G+b*B)*255);
+NRGB(:,:,2)=uint8((R/(R+G+B))*255);
+NRGB(:,:,3)=uint8((G/(R+G+B))*255);
+
+end
