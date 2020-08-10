43d59fd37ebcb915afcd326acfa0b4280db9eac0
medattia <42179315+medattia@users.noreply.github.com>
2020-08-10 21:43:56 +0100

    Update and rename skewness.m to statistical_methods.m

--- skewness.m
+++
@@ -1,5 +0,0 @@
-% out = skewness(data)
-% Compute the skewness over a dataset
-function out = skewness(data)
-	out = mean((data - mean(data) / std(data)).^3);
-end
---
+++ statistical_methods.m
@@ -0,0 +1,17 @@
+% out = skewness(data)
+% Compute the skewness over a dataset
+function out = skewness(data)
+	out = mean((data - mean(data) / std(data)).^3);
+end
+function d=union(mat1,mat2)
+[l1 c1]=size(mat1);
+[l2 c2]=size(mat2);
+mat11 = imresize(mat1, [min(l1,l2) min(c1,c2)]);
+mat22 = imresize(mat2, [min(l1,l2) min(c1,c2)]);
+d=zeros(size(mat11,1),size(mat11,2));
+for i=1:size(mat11,1);
+   for j=1:size(mat11,2);
+       d(i,j)=max(mat11(i,j),mat22(i,j)); 
+   end;
+ end;
+end
